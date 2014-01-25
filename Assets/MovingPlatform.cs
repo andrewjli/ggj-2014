ab5e6c7111b804f563f86c1848757e1aa68d289d
Guy <guydesu@gmail.com>
2014-01-25 18:07:36 +0000

    Fixed moving platform script to handle time changes

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -7,26 +7,34 @@ public class MovingPlatform : MonoBehaviour {
     public Transform startPoint;
     public float speed;
     public bool changeDirection;
+	public bool unaffectedByTimechange;
 	// Use this for initialization
 	void Start () {}
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.position == endPoint.position)
-        {
-            changeDirection = true;
-        }
-        if (transform.position == startPoint.position)
-        {
-            changeDirection = false;
-        }
-        if (changeDirection)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed);
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, speed);
-        }
+		if (unaffectedByTimechange) {
+			calculatePos();
+				}
 	}
+
+	void fixedUpdate(){
+		if (!unaffectedByTimechange) {
+			calculatePos();
+		}
+		}
+
+	void calculatePos(){
+				if (transform.position == endPoint.position) {
+						changeDirection = true;
+				}
+				if (transform.position == startPoint.position) {
+						changeDirection = false;
+				}
+				if (changeDirection) {
+						transform.position = Vector3.MoveTowards (transform.position, startPoint.position, speed);
+				} else {
+						transform.position = Vector3.MoveTowards (transform.position, endPoint.position, speed);
+				}
+		}
 }
