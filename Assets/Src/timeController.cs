e237d83e74c23b72f33c8adfcd4bd6ea02e8c49a
= <kylewhite1986@gmail.com>
2014-01-25 10:31:18 +0000

    added basic timeController onto camera

---
+++ Assets/Src/timeController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timeController : MonoBehaviour {
+
+
+    public float timeRemaining;
+    public Vector3 currentPosition;
+
+	void Start () {
+        currentPosition = this.transform.position;
+        
+
+	}
+	
+	
+	void Update () {
+
+        if (currentPosition != this.transform.position)
+            timeRemaining -= Time.deltaTime;
+
+
+        currentPosition = this.transform.position;
+
+        Debug.Log(timeRemaining);
+
+	}
+
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 170, 50), "Time Remaining: " + timeRemaining);
+    }
+}
