3bb96dd3755ffbc387371d2e329cb049243cb3b1
Guy <guydesu@gmail.com>
2014-01-26 03:02:48 +0000

    Magic time changes

--- Assets/Src/TimeLord.cs
+++ Assets/Src/TimeLord.cs
@@ -48,7 +48,7 @@ public class TimeLord : MonoBehaviour {
         }
     }
 
-	void Update()
+	void FixedUpdate()
 	{
 		if (Input.GetKeyDown (KeyCode.F1)) {
 			GameObject.Find("Main Camera").GetComponent<Camera>().transform.position = GameObject.Find("Door").GetComponent<NextlevelLoad>().transform.position;
