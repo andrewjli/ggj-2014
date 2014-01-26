d3a8067450842eeac370640f9018988425840acf
Guy <guydesu@gmail.com>
2014-01-26 02:09:12 +0000

    Button malogic added

--- Assets/Src/ButtonPress.cs
+++ Assets/Src/ButtonPress.cs
@@ -5,16 +5,29 @@ public class ButtonPress : MonoBehaviour {
 	private bool inRange = false;
 	private bool pressed = false;
 	// Use this for initialization
+	TimeLord timecont;
+
+	private float timeRemaining;
 	void Start () {
-	
+		timecont =GameObject.FindObjectOfType<TimeLord> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.E) && inRange && !pressed) {
-			StartCoroutine(buttonPress());
+		if (Input.GetKey (KeyCode.E) && inRange && !pressed) {
+			runPressed();
+					} else if (pressed) {
+			if (!timecont.isStopped())
+							timeRemaining -= Time.deltaTime;
+					}
+				
+		if (pressed && timeRemaining<=0)
+				{
+				pressed=false;
+				gameObject.transform.position += Vector3.up * 0.3f;
+				GameObject.Find("Door").GetComponent<UnlockDoor>().down();
+					}
 		}
-	}
 
 	void OnTriggerEnter (Collider other) {
 		inRange = true;
@@ -24,24 +37,25 @@ public class ButtonPress : MonoBehaviour {
 		inRange = false;
 	}
 
-	IEnumerator buttonPress() {
-		pressed = true;
-		// Button down
-		gameObject.transform.position += Vector3.down * 0.1f;
-
-		// Play sound
-		
-		// Action logic
-		GameObject.Find("Door").GetComponent<UnlockDoor>().up();
-		yield return new WaitForSeconds(10);
-		
-		// Button up
-		gameObject.transform.position += Vector3.up * 0.1f;
-		GameObject.Find("Door").GetComponent<UnlockDoor>().down();
-		pressed = false;
-	}
 
 	public bool isPressed() {
 		return pressed;
 	}
+	public void setPressed(bool a){
+		if (a) {
+						if (!pressed)
+								runPressed ();
+				} else {
+			pressed=a;
+			timeRemaining=0;
+				}
+		
+	}
+
+	void runPressed(){
+		pressed = true;
+		timeRemaining = 2.0f;
+		gameObject.transform.position += Vector3.down * 0.3f;
+		GameObject.Find("Door").GetComponent<UnlockDoor>().up();
+		}
 }
