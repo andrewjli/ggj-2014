a6b28a2127d79e7c8b166083e4778496c5a7458f
Guy <guydesu@gmail.com>
2014-01-26 03:23:42 +0000

    Ball time control fixed

---
+++ Assets/Src/MagicBall.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicBall : MonoBehaviour {
+	private TimeLord timeController;
+	bool hasBeenPaused = false;
+	Vector3 storedVelo;
+	Vector3 storedAngularVelo;
+
+
+	// Use this for initialization
+	void Start () {
+		timeController =GameObject.FindObjectOfType<TimeLord> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeController.isStopped ()) {
+			if (!hasBeenPaused)
+			{
+				storedVelo= gameObject.GetComponent<Rigidbody> ().velocity;
+				gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(0,0,0);
+				storedAngularVelo = gameObject.GetComponent<Rigidbody> ().angularVelocity;
+				gameObject.GetComponent<Rigidbody> ().angularVelocity = new Vector3(0,0,0);
+
+				gameObject.GetComponent<Rigidbody> ().Sleep();
+				gameObject.GetComponent<Rigidbody> ().useGravity = false;
+				hasBeenPaused=true;
+			}
+				} else {
+			if (hasBeenPaused)
+			{
+				hasBeenPaused=false;
+				gameObject.GetComponent<Rigidbody> ().useGravity = true;
+				gameObject.GetComponent<Rigidbody> ().WakeUp();
+				gameObject.GetComponent<Rigidbody> ().velocity = storedVelo;
+				gameObject.GetComponent<Rigidbody> ().angularVelocity = storedAngularVelo;
+
+			}
+				}
+
+	}
+}
